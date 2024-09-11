534d01898ab76c07c9dcc728cae35a640ffb7e62
Fran92p <166754292+Fran92p@users.noreply.github.com>
2024-09-11 23:19:22 +0000

    todo mucho masd de

---
+++ buttones.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('calendarButton').addEventListener('click', () => {
+        window.location.href = 'calendar.html';
+    });
+    
+});
