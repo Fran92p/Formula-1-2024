0ef79d7786ee1117b26d782df08c06169da669a4
Fran92p <166754292+Fran92p@users.noreply.github.com>
2024-09-18 23:22:14 +0000

    mucho mucho

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
 document.getElementById('redirectButton').addEventListener('click', function() {
     window.location.href = 'buttons.html';
 });
-
-document.getElementById('searchButton').addEventListener('click', function() {
-    const searchTerm = document.getElementById('searchInput').value.toLowerCase();
-    if (searchTerm.trim() !== '') {
-        removeHighlights();
-        highlightText(searchTerm);
-    } else {
-        alert('Por favor, ingresa una palabra para buscar.');
-    }
-});
-
-function highlightText(searchTerm) {
-    const elements = document.querySelectorAll('body *:not(script):not(style)');
-    elements.forEach(element => {
-        if (element.children.length === 0 && element.textContent.toLowerCase().includes(searchTerm)) {
-            const regex = new RegExp(`(${searchTerm})`, 'gi');
-            element.innerHTML = element.textContent.replace(regex, `<span class="highlight">$1</span>`);
-        }
-    });
-}
-
-function removeHighlights() {
-    const highlights = document.querySelectorAll('.highlight');
-    highlights.forEach(span => {
-        const parent = span.parentNode;
-        parent.replaceChild(document.createTextNode(span.textContent), span);
-        parent.normalize(); // Combina nodos de texto adyacentes
-    });
-}
