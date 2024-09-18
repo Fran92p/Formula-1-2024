808fa12511fa441d39a8199e64c1a5266bdb3bc8
Fran92p <166754292+Fran92p@users.noreply.github.com>
2024-09-18 22:42:42 +0000

    buscador

--- calendar.js
+++ calendar.js
@@ -37,3 +37,32 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+document.getElementById('searchButton').addEventListener('click', function() {
+    const searchTerm = document.getElementById('searchInput').value.toLowerCase();
+    if (searchTerm.trim() !== '') {
+        removeHighlights();
+        highlightText(searchTerm);
+    } else {
+        alert('Por favor, ingresa una palabra para buscar.');
+    }
+});
+
+function highlightText(searchTerm) {
+    const elements = document.querySelectorAll('body *:not(script):not(style)');
+    elements.forEach(element => {
+        if (element.children.length === 0 && element.textContent.toLowerCase().includes(searchTerm)) {
+            const regex = new RegExp(`(${searchTerm})`, 'gi');
+            element.innerHTML = element.textContent.replace(regex, `<span class="highlight">$1</span>`);
+        }
+    });
+}
+
+function removeHighlights() {
+    const highlights = document.querySelectorAll('.highlight');
+    highlights.forEach(span => {
+        const parent = span.parentNode;
+        parent.replaceChild(document.createTextNode(span.textContent), span);
+        parent.normalize(); // Combina nodos de texto adyacentes
+    });
+}
